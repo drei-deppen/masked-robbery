77471d2749717a3ef1908543311c37b4d0a0230e
Benjamin Kleiner <bizzl@benkle.info>
2026-02-01 17:35:09 +0100

    Added Win Condition

--- Game.cs
+++ Game.cs
@@ -43,4 +43,19 @@ public partial class Game : Node
 	{
 		GetNode<CanvasLayer>("HelpBox").Hide();
 	}
+
+	public async void OnWinHere(Node2D body)
+	{
+		var level1 = GetNode<Level1>("Level1");
+		level1.Stop();
+		await Task.Delay(1000);
+		await GetNode<LevelHider>("LevelHider").FadeIn();
+		RemoveChild(level1);
+		level1.QueueFree();
+		var scene = ResourceLoader.Load<PackedScene>("res://Levels/Level1.tscn").Instantiate();
+		scene.Name = "Level1";
+		AddChild(scene);
+		GetNode<Ferret>("Level1/Ferret").GameOver += OnGameOver;
+		GetNode<CanvasLayer>("GameWonUI").Show();
+	}
 }
