ae715daede187412895ce426df1092979523f3ba
Benjamin Kleiner <bizzl@benkle.info>
2026-02-01 17:54:23 +0100

    Fixes

--- Game.cs
+++ Game.cs
@@ -15,11 +15,12 @@ public partial class Game : Node
 	{
 		ScoreService.Reset();
 		GetNode<CanvasLayer>("GameOverUI").Hide();
+		GetNode<CanvasLayer>("GameWonUI").Hide();
 		await GetNode<LevelHider>("LevelHider").FadeOut();
 		GetNode<Level1>("Level1").Start();
 	}
 
-	public async void OnGameOver()
+	private async Task _stopGame()
 	{
 		var level1 = GetNode<Level1>("Level1");
 		level1.Stop();
@@ -31,6 +32,12 @@ public partial class Game : Node
 		scene.Name = "Level1";
 		AddChild(scene);
 		GetNode<Ferret>("Level1/Ferret").GameOver += OnGameOver;
+		GetNode<Area2D>("Level1/WinHere").BodyEntered += OnWinHere;
+	}
+
+	public async void OnGameOver()
+	{
+		await _stopGame();
 		GetNode<CanvasLayer>("GameOverUI").Show();
 	}
 
@@ -46,16 +53,7 @@ public partial class Game : Node
 
 	public async void OnWinHere(Node2D body)
 	{
-		var level1 = GetNode<Level1>("Level1");
-		level1.Stop();
-		await Task.Delay(1000);
-		await GetNode<LevelHider>("LevelHider").FadeIn();
-		RemoveChild(level1);
-		level1.QueueFree();
-		var scene = ResourceLoader.Load<PackedScene>("res://Levels/Level1.tscn").Instantiate();
-		scene.Name = "Level1";
-		AddChild(scene);
-		GetNode<Ferret>("Level1/Ferret").GameOver += OnGameOver;
+		await _stopGame();
 		GetNode<CanvasLayer>("GameWonUI").Show();
 	}
 }
